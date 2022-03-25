a777a3cd5e24a7363d26395a4c98e0c45d74b5df
Realdens <denis.eleyehou@hotmail.fr>
2022-03-25 09:01:19 +0100

    contrôle de données d'un formulaire / redirection url / commenter le code

--- post.php
+++ post.php
@@ -5,19 +5,15 @@
 //echo($_GET['article']);
 
 // Ce controller doit recevoir un parametre article
-if (!isset($_GET["article"])) {
-    die("Manque paramètre !");
+if (!isset($_GET["article"])) {  // La fonction isset permet de vérifier la présence (ou l'absence si précédée de "!") d'un paramètre, ici "article"
+    die("Manque paramètre !"); // L'instruction "die" met fin à l'exécution du code et affiche un message
 }
 
-include("model/post.model.php"); 
+include("model/post.model.php"); // "include" permet de charger et exécuter un modèle
 
-$bg = $data["post_img_url"];
-$layout_title = $data["post_title"];
-$header_title = substr($data["post_title"], 0, TRONCATURE_HEADER) . '...';
+$bg = $data["post_img_url"]; // Affectation à la variable bg de la variable data (ayant pour paramètre une image)
+$layout_title = $data["post_title"]; // Affectation à la variable layout_title de la variable data (ayant pour paramètre un titre)
+$header_title = substr($data["post_title"], 0, TRONCATURE_HEADER) . '...'; // Affectation à la variable header_title d'une chaine de caractère 
 $header_subtitle = '...' . substr($data["post_title"], TRONCATURE_HEADER);
 
-include("view/post.view.php"); 
-
-
-
-
+include("view/post.view.php"); // Ce second "include" charge et exécute une vue
--- view/layout/header.inc.php
+++ view/layout/header.inc.php
@@ -1,8 +1,21 @@
         <!-- Page Header-->
+        
         <header class="masthead" style="background-image: url('<?= $bg ?>')">
             <div class="container position-relative px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
+                    
+                    <?php if (isset($_GET["notif"])) { ?>
+                    <div class="alert alert-success" role="alert">
+                        <?php if ($_GET["notif"] ==1) echo "Merci de nous avoir contacté !"; ?>
+                        <?php if ($_GET["notif"] ==2) echo "Merci de vous être inscrit !"; ?>
+                    </div>
+                    <?php } ?>
+                    <?php if (isset($_GET["erreur"])) { ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php if ($_GET["erreur"] ==1) echo "Désolé, votre demande de contact a échoué !"; ?>
+                    </div>
+                    <?php } ?>
                         <div class="site-heading">
                             <h1><?= $header_title ?></h1>
                             <span class="subheading"><?= $header_subtitle ?></span>
